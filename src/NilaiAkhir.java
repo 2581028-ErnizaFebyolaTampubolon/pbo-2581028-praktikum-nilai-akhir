29b4d78ef9562c9bd47301cd1db465e4198e417b
Erniza Febyola Tampubolon <2581028@unai.edu>
2026-09-25 10:24:36 +0700

    Membuat struktur program dan input nilai

--- src/NilaiAkhir.java
+++ src/NilaiAkhir.java
@@ -23,35 +23,6 @@ public class NilaiAkhir {
         System.out.print("Nilai final : ");
         double nilaiFinal = input.nextDouble();
 
-        double akhir = praktikum * BOBOT_PRAKTIKUM
-                + tugas * BOBOT_TUGAS
-                + mid * BOBOT_MID
-                + nilaiFinal * BOBOT_FINAL;
-
-        akhir += 0;
-
-        int dipotong = (int) akhir;
-
-        int dibulatkan = (int) Math.round(akhir);
-
-        double selisih = akhir - dibulatkan;
-
-        boolean lulus = akhir >= 60;
-
-
-        System.out.println();
-        System.out.println("===== NILAI AKHIR =====");
-        System.out.printf("Praktikum : %.1f (30%%)%n", praktikum);
-        System.out.printf("Tugas     : %.1f (20%%)%n", tugas);
-        System.out.printf("MID       : %.1f (20%%)%n", mid);
-        System.out.printf("Final     : %.1f (30%%)%n", nilaiFinal);
-
-        System.out.printf("Nilai akhir       : %.1f%n", akhir);
-        System.out.println("Dipotong (int)    : " + dipotong);
-        System.out.println("Dibulatkan (round): " + dibulatkan);
-        System.out.println("Selisih            : " + selisih);
-        System.out.println("Lulus (>=60)       : " + lulus);
-
         input.close();
     }
 }
